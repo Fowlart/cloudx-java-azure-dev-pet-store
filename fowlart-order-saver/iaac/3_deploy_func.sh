2d66d8eadc11224182623c06daa20f17c9aa30f2
Artur <fowlart1988@gmail.com>
2023-08-20 01:58:37 +0300

    20.08: azure function and storage modul

--- fowlart-order-saver/iaac/3_deploy_func.sh
+++ fowlart-order-saver/iaac/3_deploy_func.sh
@@ -12,8 +12,10 @@ az storage account create --name fowlartstorageaccount  --resource-group fowlart
 # get connection string to storage account
 connectionString=$(az storage account show-connection-string --name fowlartstorageaccount --resource-group fowlartordersaver-rg --output tsv)
 
+echo "Storage account connectionString is $connectionString"
+
 # get url of deployed function
-url=$(az functionapp show --name fowlart-order-saver --resource-group fowlartordersaver-rg --output tsv --query "hostNames[0]")
+url=$(az functionapp show --name fowlartordersaver --resource-group fowlartordersaver-rg --output tsv --query "hostNames[0]")
 
 # set connection string as app setting in function app
 az webapp config appsettings set --name fowlartordersaver --resource-group fowlartordersaver-rg --settings BLOB_CONNECTION_STRING=$connectionString
